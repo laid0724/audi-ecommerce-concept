bdf1c6361f4db539b1d5bb6df94c96563cfec272
David Lai <laid0724@gmail.com>
2021-06-25 14:26:37 +0800

    IMPL .net: add extensions to CLaimsPrincipal to get username and userid in requests

---
+++ server/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,21 @@
+using System.Security.Claims;
+
+namespace Audi.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        /*
+            we can access something called Claims from an http request, where we can extract and read the current
+            user sending the request as a ClaimsPrincipal, to read more, see:
+            https://docs.microsoft.com/en-us/dotnet/api/system.security.claims.claimsprincipal?view=net-5.0
+        */
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
+    }
+}
