525207b57e7a5a4a602223d156e762f89849fb75
David Lai <laid0724@gmail.com>
2021-06-25 12:55:08 +0800

    ADD angular: scaffold project skeleton structure

---
+++ client/projects/data/src/lib/data.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'audi-data-data',
+  template: `
+    <p>
+      data works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
