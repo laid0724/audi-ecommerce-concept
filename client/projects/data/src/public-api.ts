bee1fc7447c2ce59fabea1675c1572beeedd7c9a
David Lai <laid0724@gmail.com>
2021-06-26 21:01:31 +0800

    UPDT angular: clean up data library and declare public apis

--- client/projects/data/src/public-api.ts
+++ client/projects/data/src/public-api.ts
@@ -2,6 +2,25 @@
  * Public API Surface of data
  */
 
-export * from './lib/data.service';
-export * from './lib/data.component';
-export * from './lib/data.module';
+// Models
+export * from './lib/models/pagination';
+export * from './lib/models/users';
+
+// Services
+export * from './lib/services/account.service'
+export * from './lib/services/busy.service'
+
+// Interceptors
+export * from './lib/interceptors/error.interceptor'
+export * from './lib/interceptors/jwt.interceptor'
+export * from './lib/interceptors/loading.interceptor'
+
+// Guards
+export * from './lib/guards/auth.guard'
+export * from './lib/guards/admin.guard'
+export * from './lib/guards/member.guard'
+
+export * from './lib/pipes/pipes.module';
+export * from './lib/regex';
+export * from './lib/validators';
+export * from './lib/helpers';
