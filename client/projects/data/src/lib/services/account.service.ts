ea8d599a2fd6cc7d5f4f39dffd593a7eed91ce75
David Lai <laid0724@gmail.com>
2021-06-26 21:00:22 +0800

    ADD angular: shared services to data library

---
+++ client/projects/data/src/lib/services/account.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Role, User } from '../models/users';
+
+interface UserCredential {
+  userName: string;
+  password: string;
+}
+
+interface JwtToken {
+  nameid: string;
+  unique_name: string;
+  role: string | string[];
+  nbf: number;
+  exp: number;
+  iat: number;
+}
+
+@Injectable({
+  // if set to root, no longer need to add into providers array in modules in newer versions of angular
+  providedIn: 'root',
+})
+export class AccountService {
+  private endpoint = '/api/account';
+
+  private currentUserSource = new ReplaySubject<User>(1); // only store one value from the stream when next is triggered
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  setCurrentUser(user: User): void {
+    if (user !== null) {
+      user.roles = [];
+      const roles = this.getDecodedToken(user.token).role as Role | Role[];
+      Array.isArray(roles) ? (user.roles = roles) : user.roles.push(roles);
+    }
+
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
+
+  register(userCredential: UserCredential): Observable<User> {
+    return this.http
+      .post<User>(this.endpoint + '/register', userCredential)
+      .pipe(
+        tap((user: User) => {
+          if (user) {
+            this.setCurrentUser(user);
+          }
+        })
+      );
+  }
+
+  login(userCredential: UserCredential): Observable<User> {
+    return this.http.post<User>(this.endpoint + '/login', userCredential).pipe(
+      tap((user: User) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  getDecodedToken(token: string): JwtToken {
+    // atob is a method that allows us to decrypt the parts of a JWT token that does not require a signature
+    // [1] refers to the payload property inside a JWT token
+    return JSON.parse(atob(token.split('.')[1]));
+  }
+}
