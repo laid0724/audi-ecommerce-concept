4738fec7cf3521bb9cf5b7a69700e25088f4a77d
David Lai <laid0724@gmail.com>
2021-06-28 15:48:46 +0800

    UPDT angular sys serve-error component to catch server errors in dev mode

--- client/projects/sys/src/app/core/server-error/server-error.component.ts
+++ client/projects/sys/src/app/core/server-error/server-error.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'projects/sys/src/environments/environment';
 
 @Component({
   selector: 'audi-sys-server-error',
   templateUrl: './server-error.component.html',
-  styleUrls: ['./server-error.component.scss']
+  styleUrls: ['./server-error.component.scss'],
 })
-export class ServerErrorComponent implements OnInit {
+export class ServerErrorComponent {
+  isProductionEnv: boolean = environment.production;
+  error: any;
 
-  constructor() { }
+  constructor(private router: Router) {
+    /*
+      IMPORTANT: the state of the router can only be accessed INSIDE the constructor, not in OnInit!
 
-  ngOnInit(): void {
+      Note: refreshing this page will make the error disappear because the state of the router is derived
+            via the previous router action.
+    */
+
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.error;
   }
-
 }
