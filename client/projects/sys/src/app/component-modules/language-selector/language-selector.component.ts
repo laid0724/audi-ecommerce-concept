dfacef9ca2cfa6c7621246274b3d74684ccf49e7
David Lai <laid0724@gmail.com>
2021-06-30 22:33:48 +0800

    RFX language-selector outsource clear query params function

--- client/projects/sys/src/app/component-modules/language-selector/language-selector.component.ts
+++ client/projects/sys/src/app/component-modules/language-selector/language-selector.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Data, Router } from '@angular/router';
-import { LanguageCode, LanguageStateService } from '@audi/data';
+import {
+  setQueryParams,
+  LanguageCode,
+  LanguageStateService,
+} from '@audi/data';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -32,19 +36,8 @@ export class LanguageSelectorComponent implements OnInit {
     );
   }
 
-  clearQueryParams(): void {
-    // currently, no native angular method to clear query params on the same page
-    // this is a workaround to that
-    // see: https://stackoverflow.com/questions/48552993/angular-5-remove-query-param
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: {},
-      queryParamsHandling: '',
-    });
-  }
-
   onChangeLanguage(language: LanguageCode): void {
     this.languageState.selectLanguage(language);
-    this.clearQueryParams();
+    setQueryParams(this.router, this.route);
   }
 }
