5f9f25af51476cd98a2cf4512bb4e4827135ad5a
David Lai <laid0724@gmail.com>
2021-08-01 22:32:25 +0800

    UPDT OrderItemDto

--- server/Audi/DTOs/OrderItemDto.cs
+++ server/Audi/DTOs/OrderItemDto.cs
@@ -3,12 +3,8 @@ namespace Audi.DTOs
     public class OrderItemDto
     {
         public int Id { get; set; }
-        public int OrderId { get; set; }
-        public int UserId { get; set; }
-        public int ProductId { get; set; }
         public ProductDto Product { get; set; }
         public int VariantValueId { get; set; }
-        public ProductVariantValueDto ProductVariantValue { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
     }
