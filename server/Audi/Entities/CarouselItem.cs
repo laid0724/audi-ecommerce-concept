74d71518105f9c07da7f490608349b5194c75ab2
David Lai <laid0724@gmail.com>
2021-08-08 17:09:54 +0800

    ADD homepage entitites migration

--- server/Audi/Entities/CarouselItem.cs
+++ server/Audi/Entities/CarouselItem.cs
@@ -11,7 +11,7 @@ namespace Audi.Entities
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Body { get; set; }
-        public bool IsActive { get; set; } = false;
+        public bool IsVisible { get; set; } = false;
         public string PrimaryButtonLabel { get; set; }
         public string PrimaryButtonUrl { get; set; }
         public string SecondaryButtonLabel { get; set; }
