0564fbbde6d810fd0eba212d8f80c7634579dee2
David Lai <laid0724@gmail.com>
2021-08-10 01:54:15 +0800

    IMPL has duplicate value validator for form arrays

--- client/projects/data/src/lib/helpers.ts
+++ client/projects/data/src/lib/helpers.ts
@@ -76,6 +76,10 @@ export function isNullOrEmptyString(val: string | null | undefined): boolean {
   return val == null || (val && val.trim() === '') || val.length === 0;
 }
 
+export function hasDuplicates(array: any[]): boolean {
+  return new Set(array).size !== array.length;
+}
+
 export function stringToBoolean(string: string) {
   return string === 'false' ? false : !!string;
 }
