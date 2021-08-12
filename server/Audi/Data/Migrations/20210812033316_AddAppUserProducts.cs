e6b1bf5667ac20fd984f9642db48e2c147a6c3a4
David Lai <laid0724@gmail.com>
2021-08-12 12:56:52 +0800

    ADD migrations for AppUserProducts

---
+++ server/Audi/Data/Migrations/20210812033316_AddAppUserProducts.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Data.Migrations
+{
+    public partial class AddAppUserProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "app_user_products",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    product_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_app_user_products", x => new { x.user_id, x.product_id });
+                    table.ForeignKey(
+                        name: "FK_app_user_products_idt_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "idt_users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_app_user_products_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_app_user_products_product_id",
+                table: "app_user_products",
+                column: "product_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "app_user_products");
+        }
+    }
+}
