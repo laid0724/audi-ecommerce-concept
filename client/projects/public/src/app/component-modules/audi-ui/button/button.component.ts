50e981c8f03ca0eda5298ea21f473ed48cd0380e
David Lai <laid0724@gmail.com>
2021-08-23 21:22:09 +0800

    FIX audi btn & add documentation

--- client/projects/public/src/app/component-modules/audi-ui/button/button.component.ts
+++ client/projects/public/src/app/component-modules/audi-ui/button/button.component.ts
@@ -2,6 +2,50 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AudiModuleName, initAudiModules } from '@audi/data';
 import { AudiButton } from '../enums';
 
+/*
+  USAGE:
+
+  icon btn:
+  <audi-button type="icon">
+    <audi-icon size="small" iconName="system-download"></audi-icon>
+  </audi-button>
+
+  large icon btn:
+  <audi-button type="icon-large">
+    <audi-icon size="large" iconName="system-download"></audi-icon>
+  </audi-button>
+
+  btn primary
+  <audi-button type="primary">
+    Button
+  </audi-button>
+
+  btn secondary
+  <audi-button type="secondary">
+    Button
+  </audi-button>
+
+  btn text
+  <audi-button type="text">
+    Button
+  </audi-button>
+
+  btn active
+  <audi-button type="primary" [isActive]="true">
+    Button
+  </audi-button>
+
+  btn disabled
+  <audi-button type="primary" [isDisabled]="true">
+    Button
+  </audi-button>
+
+  btn light theme
+  <audi-button type="primary" [isLightTheme]="true">
+    Button
+  </audi-button>
+*/
+
 @Component({
   selector: 'audi-button',
   templateUrl: './button.component.html',
