f2578ce521d001067008d413cbedef2db302210f
David Lai <laid0724@gmail.com>
2021-08-24 17:16:53 +0800

    IMPL default slider module

--- client/projects/public/src/app/component-modules/slider/default-slider/default-slider.component.ts
+++ client/projects/public/src/app/component-modules/slider/default-slider/default-slider.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ContentChildren,
+  Input,
+  QueryList,
+  ViewChild,
+} from '@angular/core';
+import { ProjectAsTemplateDirective } from '@audi/data';
+
+// Documentation:
+// https://swiperjs.com/angular
+// Api Documentation:
+// https://swiperjs.com/swiper-api
+import { SwiperComponent } from 'swiper/angular';
+
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Autoplay, Swiper } from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Autoplay]);
+
+/*
+
+  USAGE:
+  <audi-default-slider
+    [autoplay]="true"
+    [isLightTheme]="false"
+    [indicatorOnImage]="true"
+    [indicatorIsShadowed]="false"
+  >
+    SLIDE HERE
+    CAN BE ANY ELEMENT BUT YOU MUST PIN *projectAsTemplate DIRECTIVE
+    <img
+      *projectAsTemplate
+      src="imgUrl"
+    />
+  </audi-default-slider>
+
+*/
 
 @Component({
   selector: 'audi-default-slider',
   templateUrl: './default-slider.component.html',
-  styleUrls: ['./default-slider.component.scss']
+  styleUrls: ['./default-slider.component.scss'],
 })
-export class DefaultSliderComponent implements OnInit {
+export class DefaultSliderComponent implements AfterViewInit {
+  @ViewChild('swiper', { static: false }) swiper: SwiperComponent;
 
-  constructor() { }
+  // see: https://stackoverflow.com/questions/51506636/how-can-i-use-contentchildren-and-only-render-a-specific-child-component-instea
+  @ContentChildren(ProjectAsTemplateDirective)
+  slides: QueryList<ProjectAsTemplateDirective>;
 
-  ngOnInit(): void {
+  @Input() activeSlide: number = 0;
+  @Input() autoplay: boolean = false;
+  @Input() autoplayTransitionTime: number = 3000;
+
+  @Input() isLightTheme: boolean = false;
+  @Input() indicatorOnImage: boolean = false;
+  @Input() indicatorIsShadowed: boolean = false;
+
+  swiperInstance: Swiper;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.swiperInstance = this.swiper.swiperRef as Swiper;
   }
 
+  onActiveSlideChange(swiper: any): void {
+    const { activeIndex } = swiper as Swiper;
+    if (this.swiperInstance != null) {
+      this.activeSlide = activeIndex;
+
+      // swiper and audi ui runs outside of ngZone, so manual change detect trigger is needed
+      this.cdr.detectChanges();
+    }
+  }
+
+  slideTo(i: number): void {
+    this.swiperInstance.slideTo(i);
+    this.activeSlide = i;
+  }
 }
--- client/projects/public/src/app/component-modules/slider/default-slider/default-slider.module.ts
+++ client/projects/public/src/app/component-modules/slider/default-slider/default-slider.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SwiperModule } from 'swiper/angular';
 import { DefaultSliderComponent } from './default-slider.component';
 import { AudiUiModule } from '../../audi-ui/audi-ui.module';
+import { DirectivesModule } from '@audi/data';
 
 const COMPONENTS = [DefaultSliderComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, SwiperModule, AudiUiModule],
-  exports: [...COMPONENTS],
+  imports: [CommonModule, SwiperModule, AudiUiModule, DirectivesModule],
+  exports: [...COMPONENTS, DirectivesModule],
 })
 export class DefaultSliderModule {}
