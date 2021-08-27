c557ebe33bae2d0f8a18b8e10a6618883d8d4a27
David Lai <laid0724@gmail.com>
2021-08-27 16:31:59 +0800

    uninstall lodash

--- client/projects/public/src/typings/lodash.d.ts
+++
@@ -1,6 +0,0 @@
-/*
-  i am not installing @types/lodash because its function names
-  overlaps with RxJs, such as tap, map, etc, which messes up my
-  intellisense and making it annoying as fuck to import the correct one.
-*/
-declare module 'lodash';
