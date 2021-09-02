5476d112c32d1584a0ebf3ca4592259ffc5b7d53
David Lai <laid0724@gmail.com>
2021-09-02 18:08:55 +0800

    IMPL fixed header bg for products-list

--- client/projects/public/src/app/feature-modules/products/products-list/products-list.component.ts
+++ client/projects/public/src/app/feature-modules/products/products-list/products-list.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { ProductsService } from '@audi/data';
 
 @Component({
   selector: 'audi-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  styleUrls: ['./products-list.component.scss'],
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('headerBg') headerBg: ElementRef<HTMLDivElement>;
 
-  constructor() { }
+  windowScrollListenerFn: () => void;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private productsService: ProductsService
+  ) {}
 
   ngOnInit(): void {
+
   }
 
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      /*
+        using background-attachment: fixed ALONG with background-size: cover
+        makes the background bg size all messed up.
+
+        this is a workaround to that issue, mimicking parallax effect.
+
+        see: https://stackoverflow.com/questions/21786272/css-background-size-cover-background-attachment-fixed-clipping-background-im
+      */
+
+      this.windowScrollListenerFn = this.renderer.listen(
+        window,
+        'scroll',
+        (event: any) => {
+          // see: see: https://stackoverflow.com/questions/28050548/window-pageyoffset-is-always-0-with-overflow-x-hidden
+          const scrollTop = event.srcElement.scrollingElement.scrollTop;
+
+          this.renderer.setStyle(
+            this.headerBg.nativeElement,
+            'transform',
+            `translateY(${scrollTop}px)`
+          );
+        }
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId) && this.windowScrollListenerFn) {
+      this.windowScrollListenerFn();
+    }
+  }
 }
--- client/projects/public/src/app/feature-modules/products/products.module.ts
+++ client/projects/public/src/app/feature-modules/products/products.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProductsRoutingModule } from './products-routing.module';
+import { TranslocoModule } from '@ngneat/transloco';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+import { PaginationModule } from '../../component-modules/audi-ui/pagination/pagination.module';
+import { ButtonModule } from '../../component-modules/audi-ui/button/button.module';
+import { IconModule } from '../../component-modules/audi-ui/icon/icon.module';
+import { SliderModule } from '../../component-modules/slider/slider.module';
+import { FooterModule } from '../../core/footer/footer.module';
+import { NavModule } from '../../component-modules/audi-ui/nav/nav.module';
+
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductsSingleComponent } from './products-single/products-single.component';
+
+const AUDI_MODULES = [
+  SliderModule,
+  IconModule,
+  ButtonModule,
+  PaginationModule,
+  NavModule,
+];
 
 const COMPONENTS = [ProductsListComponent, ProductsSingleComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, ProductsRoutingModule],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    FooterModule,
+    TranslocoModule,
+    AngularFullpageModule,
+    ...AUDI_MODULES,
+  ],
   exports: [...COMPONENTS],
 })
 export class ProductsModule {}
