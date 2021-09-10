6d413c24d07fad63726e45226845826dfcdfe67c
David Lai <laid0724@gmail.com>
2021-09-10 18:58:33 +0800

    IMPL cart item UI

---
+++ client/projects/public/src/app/feature-modules/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { CartItem, Product } from '@audi/data';
+import { isProductDiscounted } from '../../../helpers';
+import { CartService } from '../services/cart.service';
+
+@Component({
+  selector: 'audi-cart-item',
+  templateUrl: './cart-item.component.html',
+  styleUrls: ['./cart-item.component.scss'],
+})
+export class CartItemComponent {
+  @Input() cartItem: CartItem;
+
+  public isDiscounted: (product: Product) => boolean = isProductDiscounted;
+
+  get price(): number {
+    return 0;
+  }
+
+  get productMainPhotoUrl(): string {
+    if (
+      this.cartItem.product.photos &&
+      Array.isArray(this.cartItem.product.photos) &&
+      this.cartItem.product.photos.length > 0
+    ) {
+      const mainPhoto = this.cartItem.product.photos.find((p) => p.isMain);
+      if (mainPhoto) {
+        return mainPhoto.url;
+      }
+    }
+
+    return 'assets/images/placeholder-original.png';
+  }
+
+  constructor(private cartService: CartService) {}
+
+  removeItem(): void {
+    this.cartService.removeFromCart(this.cartItem);
+  }
+}
--- client/projects/public/src/app/feature-modules/cart/cart.module.ts
+++ client/projects/public/src/app/feature-modules/cart/cart.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from '../../component-modules/audi-ui/button/button.module';
 import { IconModule } from '../../component-modules/audi-ui/icon/icon.module';
+import { TranslocoModule } from '@ngneat/transloco';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { CartNavComponent } from './cart-nav/cart-nav.component';
-import { TranslocoModule } from '@ngneat/transloco';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { CartItemQuantitySelectorComponent } from './cart-item-quantity-selector/cart-item-quantity-selector.component';
 
-const COMPONENTS = [CartPageComponent, CartNavComponent];
+const COMPONENTS = [
+  CartPageComponent,
+  CartNavComponent,
+  CartItemComponent,
+  CartItemQuantitySelectorComponent,
+];
 
 const IMPORT_MODULES = [
   CommonModule,
