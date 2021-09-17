bd45649505728088b538fc4ec3f82327119b16eb
David Lai <laid0724@gmail.com>
2021-09-17 18:27:27 +0800

    IMPL order guard for orders-single

--- client/projects/public/src/app/feature-modules/members-area/guards/order.guard.ts
+++ client/projects/public/src/app/feature-modules/members-area/guards/order.guard.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
+import { AccountService, Order, OrdersService, User } from '@audi/data';
+import { Observable, of } from 'rxjs';
+import { map, switchMap, take } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(
+    private accountService: AccountService,
+    private orderService: OrdersService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    const routeHasOrderId = route.paramMap.has('orderId');
+
+    if (!routeHasOrderId) return of(false);
+
+    const orderId = +route.paramMap.get('orderId')!;
+
+    return this.accountService.currentUser$.pipe(
+      take(1),
+      switchMap((user: User | null) => {
+        if (user === null) return of(false);
+
+        const { id } = user;
+
+        return this.orderService
+          .getOrdersByUserId(id)
+          .pipe(
+            map((orders: Order[]) =>
+              orders.map(({ id }) => id).includes(orderId)
+            )
+          );
+      })
+    );
   }
-  
 }
